84868fcc2d7905698e4f0da35d069f00815a13f2
Calboot <113321314+Calboot@users.noreply.github.com>
2023-12-15 10:32:22 +0800

    将清除单个世界的掉落物更改为清除所有已加载区域的掉落物

--- src/main/java/com/calboot/cmdutil/commands/KillItemCommand.java
+++ src/main/java/com/calboot/cmdutil/commands/KillItemCommand.java
@@ -6,8 +6,10 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.TranslatableText;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class KillItemCommand {
@@ -25,8 +27,11 @@ public class KillItemCommand {
     }
 
     public static int killItem(CommandContext<ServerCommandSource> context) {
-        ServerCommandSource source = context.getSource();
-        final List<? extends ItemEntity> targets = source.getWorld().getEntitiesByType(EntityType.ITEM, itemEntity -> true);
+        final ServerCommandSource source = context.getSource();
+        final List<ItemEntity> targets = new ArrayList<>();
+        for(final ServerWorld world:source.getServer().getWorlds()){
+            targets.addAll(world.getEntitiesByType(EntityType.ITEM, itemEntity -> true));
+        }
         final int size = targets.size();
         if (size == 0) {
             source.sendFeedback(new TranslatableText("commands.cmdutil.killitem.no_target"), true);
